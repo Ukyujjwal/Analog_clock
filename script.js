f32b31ee4f4f79d2dcb8294949b2be4fc51a1bae
Ukyujjwal <ujjwalyadav40@gmail.com>
2022-06-05 16:17:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+setInterval(() => {
+d = new Date(); //object of date()
+hr = d.getHours();
+min = d.getMinutes();
+sec = d.getSeconds();
+
+// 60 min = 360
+// 1 min = 360/60 = 6
+//  hour hand  
+
+hr_rotation = 30 * hr + min / 2; //converting current time
+min_rotation = 6 * min;
+sec_rotation = 6 * sec;
+
+hour = document.getElementById("hour_hand");
+minute = document.getElementById("minute_hand");
+second = document.getElementById("second_hand");
+
+hour.style.transform = `rotate(${hr_rotation}deg)`;
+minute.style.transform = `rotate(${min_rotation}deg)`;
+second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
